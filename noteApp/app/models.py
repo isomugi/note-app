498c9040a23c4598b25d2787912d058bd64b5b66
磯上　紡 <issotsumugi@gmail.com>
2023-01-11 12:37:25 +0900

    commit

--- noteApp/app/models.py
+++ noteApp/app/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Auther(models.Model):
     name = models.CharField(max_length=20)
+    create = models.DateTimeField(auto_now_add=True)
+    saved = models.DateTimeField(auto_now=True)
 
 class Note(models.Model):
-    date = models.DateTimeField
+    date = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=200)
